9d62e4c8ece36ff3a0d4bcbac8c32f4dda29e608
Askar Bisembaev <superdeveloper333@gmail.com>
2022-06-17 23:12:40 +0500

    Добавьте файлы проекта.

---
+++ NorthwindConsoleTransaction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace NorthwindConsoleTransaction
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			string connectionString = "Data Source = .\\sqlexpress; Initial Catalog=Northwind; Integrated Security = SSPI";
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				SqlTransaction transaction = connection.BeginTransaction();
+
+				SqlCommand command = connection.CreateCommand();
+				command.Transaction = transaction;
+
+				try
+				{
+					command.CommandText = "INSERT INTO Categories (CategoryName) VALUES('Tim')";
+					command.ExecuteNonQuery();
+					command.CommandText = "INSERT INTO Employees (LastName, FirstName) VALUES('Vasya', 'Pupkin')";
+					command.ExecuteNonQuery();
+
+					transaction.Commit();
+					Console.WriteLine("Данные добавлены в базу данных");
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					transaction.Rollback();
+				}
+			}
+			Console.Read();
+		}
+	}
+}
